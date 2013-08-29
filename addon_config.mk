a9d6e1a4d583dcab0be0ca07a31844df2087bed9
Christopher Baker <me@christopherbaker.net>
2013-08-29 16:38:22 -0500

    Added config.

---
+++ addon_config.mk
@@ -0,0 +1,6 @@
+meta:
+	ADDON_NAME = ofxICMP
+	ADDON_DESCRIPTION = ofxICMP is a pure Poco addon for making ICMP (aka PING) requests
+	ADDON_AUTHOR = bakercp
+	ADDON_TAGS = "ping" "icmp"
+	ADDON_URL = https://github.com/bakercp/ofxICMP
